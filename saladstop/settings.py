89c02b80c63352740cd175932ea683a831e10a3c
Vimaniya Rabindranathan <vimaniyar@saksglobal.com>
2022-03-21 22:44:03 +0530

    mongo db

--- saladstop/settings.py
+++ saladstop/settings.py
@@ -73,11 +73,13 @@ WSGI_APPLICATION = 'saladstop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+            'ENGINE': 'djongo',
+            'NAME': 'mydb',
+      
+        }
 }
 
 
