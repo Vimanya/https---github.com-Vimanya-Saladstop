6a170573cecd753e77c8fffe8b264b73ec395a02
Vimaniya Rabindranathan <vimaniyar@saksglobal.com>
2022-03-21 23:35:18 +0530

    filter the transaction date

--- app_1/views.py
+++ app_1/views.py
@@ -9,8 +9,17 @@ from app_1.models import salad_model
 @csrf_exempt
 def index(request):
     global res_filter
-
-    res_filter = salad_model.objects.all()
-    context ={'results':res_filter}
-    return render(request,'index.html',context)
+    if request.method == 'POST':
+        fromdate = request.POST.get('fromdate')
+        todate = request.POST.get('todate')
+        fromdate = datetime.strptime(fromdate, '%Y-%m-%d')
+        todate = datetime.strptime(todate, '%Y-%m-%d')
+        print(fromdate,todate)
+        res_filter = salad_model.objects.filter(Transaction_date__range =[fromdate,todate])
+        context ={'results':res_filter}
+        return render(request,'index.html',context)
+    else:
+        res_filter = salad_model.objects.all()
+        context ={'results':res_filter}
+        return render(request,'index.html',context)
 
