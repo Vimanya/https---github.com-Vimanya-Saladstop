d7a11d47cc602343bf8d52e1d9e71eb09a6d5fd0
Vimaniya Rabindranathan <vimaniyar@saksglobal.com>
2022-03-21 23:10:23 +0530

    load data to html

--- app_1/models.py
+++ app_1/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class salad_model(models.Model):
+    Transaction_date = models.DateField(db_column='Transaction date')
+    Supplier = models.CharField(max_length=20)
+    Item_description = models.CharField(max_length=20, db_column='Item description')
+    Country_of_origin = models.CharField(max_length=20, db_column='Country of origin')
+    Quantity = models.IntegerField(db_column='Quantity')
+    Unitprice_SGD = models.FloatField(db_column='Unitprice - SGD')
+    Price_Incl_Tax = models.FloatField(db_column='Price Incl Tax')
+    Discount = models.FloatField(db_column='Discount %')
+    Total = models.FloatField(null=False)
+    GST = models.FloatField(db_column='GST')
+    Total_incl_GST = models.FloatField(db_column='Total incl GST')
+
+
+    class Meta:
+        db_table = 'stopsalad'
--- app_1/views.py
+++ app_1/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from datetime import datetime
+from app_1.models import salad_model
 
 # Create your views here.
+
+
+@csrf_exempt
+def index(request):
+    global res_filter
+
+    res_filter = salad_model.objects.all()
+    context ={'results':res_filter}
+    return render(request,'index.html',context)
+
